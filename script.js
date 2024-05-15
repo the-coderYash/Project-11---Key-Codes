3d913c5e60f7d0d48a388fb77e66a4fb9bae48da
Yash Raj <yashraj00112@gmail.com>
2024-05-15 12:14:50 +0530

    //Fixing Button

---
+++ script.js
@@ -0,0 +1,20 @@
+const insert = document.getElementById('insert')
+
+
+window.addEventListener('keydown', (e) => {
+          insert.innerHTML = `
+          <div class="key">
+                              ${e.key}
+                              <small>event.key</small>
+                    </div>
+                    <div class="key">
+                              65
+                              <small>event.keyCode</small>
+                    </div>
+
+                    <div class="key">
+                              KeyA
+                              <small>event.code</small>
+                    </div>
+                    `
+})
